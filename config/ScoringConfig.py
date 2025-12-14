8ed424b38575cb8ade49ec9d3806d6fdbb641c29
Jon Sweeney <jonsweeney26@gmail.com>
2025-12-14 09:55:20 -0800

    add default scoring config and refactor data processing file paths

---
+++ config/ScoringConfig.py
@@ -0,0 +1,81 @@
+league_default_scoring_config = {
+    "passing": {
+        "py": 0.04,
+        "ptd": 6,
+        "int": -2,
+        "2pc": 2
+    },
+    "rushing": {
+        "ry": 0.1,
+        "rtd": 6,
+        "2pr": 2
+    },
+    "receiving": {
+        "rey": 0.1,
+        "retd": 6,
+        "2pre": 2
+    },
+    "kicking": {
+        "pat": 1,
+        "patm": -1,
+        "fgm": -1,
+        "fg0": 3,
+        "fg40": 4,
+        "fg50": 6,
+        "fg60": 8
+    },
+    "tm_df_sp_tm": {
+        "sk": 1,
+        "inttd": 6,
+        "frtd": 6,
+        "krtd": 6,
+        "prtd": 6,
+        "blkkrtd": 6,
+        "blkk": 2,
+        "int": 2,
+        "fr": 2,
+        "ff": 1,
+        "sf": 2,
+        "pa0": 8,
+        "pa1": 6,
+        "pa7": 5,
+        "pa14": 3,
+        "pa18": 2,
+        "pa22": 1,
+        "pa35": -1,
+        "pa46": -5,
+        "ya100": 8,
+        "ya199": 6,
+        "ya299": 5,
+        "ya349": 3,
+        "ya399": 2,
+        "ya499": -1,
+        "ya549": -2,
+        "ya550": -4,
+        "2ptret": 2,
+        "1psf": 1
+    },
+    "misc": {
+        "krtd": 6,
+        "prtd": 6,
+        "ftd": 6,
+        "fuml": -2,
+        "inttd": 6,
+        "frtd": 6,
+        "blkkrtd": 6,
+        "2ptret": 2,
+        "1psf": 1
+    },
+    "def_plyr": {
+        "sk": 4,
+        "tk": 1,
+        "blkk": 5,
+        "int": 5,
+        "fr": 5,
+        "ff": 4,
+        "sf": 2,
+        "tka": 0.75,
+        "tks": 1.5,
+        "pd": 1.5
+    }
+}
